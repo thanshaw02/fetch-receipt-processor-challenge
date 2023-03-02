f6bd7e158bd58c623e5e575817ff44305ed41e13
Tylor Hanshaw <tylorjhanshaw@gmail.com>
2023-03-02 16:47:37 -0500

    handling form data sent as a request

--- receipt-api.go
+++ receipt-api.go
@@ -16,10 +16,18 @@ import (
 	
 	Query param extraction:
 		- (*http.Request).Has("QUERY_STRING_HERE") --> returns a boolean if the query string is present
-		- (*http.Request).Get("QUERY_STRING_HERE") --> returns a string using the query string name passed, if not found returns an empty string
+		- (*http.Request).Get("QUERY_STRING_HERE") --> returns a string using the query string name passed
+			- returns a string value if the query string value is found
+			- returns an empty string if the key query string was found but not value was given
 	
 	Body data extraction:
 		- ioutil.ReadAll((*http.Request).Body) --> reads all of the bytes from the requests Body and returns it
+
+	Form data extraction:
+		- (*http.Request).PostFormValue("FORM_VALUE_NAME") --> returns the value with the given name when submitted as a form
+			- this works with a POST, PATH, or PUT requests
+			- returns an empty string if not key was found with the key we provide
+		- (*http.Request).FormValue("FORM_VALUE_NAME") --> returns the value with the given name when submitted as a form AND any values in the query string
 */
 
 const keyServerAddr = "server-address"
@@ -52,7 +60,12 @@ func getHello(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
 	fmt.Printf("%s: got /hello request\n", ctx.Value(keyServerAddr))
-	io.WriteString(w, "Hello, HTTP!\n")
+
+	myName := r.PostFormValue("myName")
+	if myName == "" {
+		myName = "HTTP"
+	}
+	io.WriteString(w, fmt.Sprintf("Hello, %s!\n", myName))
 }
 
 func main() {
