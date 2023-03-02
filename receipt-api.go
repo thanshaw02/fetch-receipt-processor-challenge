efe607be31db6dc8d420454b97fc4da2dd9ee9a1
Tylor Hanshaw <tylorjhanshaw@gmail.com>
2023-03-02 16:38:06 -0500

    extracting a requests Body data

--- receipt-api.go
+++ receipt-api.go
@@ -5,7 +5,7 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	// "io/ioutil"
+	"io/ioutil"
 	"net"
 	"net/http"
 	// "os"
@@ -14,8 +14,12 @@ import (
 /*
 	NOTES:
 	
-	- (*http.Request).Has("QUERY_STRING_HERE") --> returns a boolean if the query string is present
-	- (*http.Request).Get("QUERY_STRING_HERE") --> returns a string using the query string name passed, if not found returns an empty string
+	Query param extraction:
+		- (*http.Request).Has("QUERY_STRING_HERE") --> returns a boolean if the query string is present
+		- (*http.Request).Get("QUERY_STRING_HERE") --> returns a string using the query string name passed, if not found returns an empty string
+	
+	Body data extraction:
+		- ioutil.ReadAll((*http.Request).Body) --> reads all of the bytes from the requests Body and returns it
 */
 
 const keyServerAddr = "server-address"
@@ -23,15 +27,23 @@ const keyServerAddr = "server-address"
 func getRoot(w http.ResponseWriter, r *http.Request) {
 	ctx := r.Context()
 
+	// fetching query params from request
 	hasFirst := r.URL.Query().Has("first")
 	first := r.URL.Query().Get("first")
 	hasSecond := r.URL.Query().Has("second")
 	second := r.URL.Query().Get("second")
 
-	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s\n",
+	// fetching Body data from request
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		fmt.Printf("could not read request body: %s\n", err)
+	}
+
+	fmt.Printf("%s: got / request. first(%t)=%s, second(%t)=%s, body:\n%s\n",
 		ctx.Value(keyServerAddr),
 		hasFirst, first,
 		hasSecond, second,
+		body,
 	)
 	io.WriteString(w, "This is my website!\n")
 }
